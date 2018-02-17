958336e771b5abb0153238e80c2bdcfc39ed843f
AllisonBrink <35696052+AllisonBrink@users.noreply.github.com>
2018-02-16 20:44:15 -0600

    Finished all methods

--- ALife/src/Cooperator.java
+++ ALife/src/Cooperator.java
@@ -1,25 +1,29 @@
 
-public class Cooperator extends Organism{
+public class Cooperator extends Organism {
 
-	private int energy;
 	public double coopProb;
-	
+
 	public Cooperator() {
-		
+		super();
+		this.coopProb = 1;
 	}
-	
+
 	public String getType() {
 		return "Cooperator";
 	}
-	
+
 	public Organism reproduce() {
-		
+		Cooperator newOrg = new Cooperator();
+		for (int i = 0; i < 10; i++) {
+			this.decrementEnergy();
+		}
+		return newOrg;
 	}
-	
+
 	public double getCooperationProbability() {
 		return 1;
 	}
-	
+
 	public boolean cooperates() {
 		return true;
 	}
