6d87e6dad6c91647f3380c357cf0abedd5684714
AllisonBrink <35696052+AllisonBrink@users.noreply.github.com>
2018-02-16 20:44:34 -0600

    Finished all methods

--- ALife/src/Defector.java
+++ ALife/src/Defector.java
@@ -1,24 +1,28 @@
 
-public class Defector extends Organism{
-	private int energy;
+public class Defector extends Organism {
+	// private int energy;
 	public double coopProb;
-	
+
 	public Defector() {
-		
+		this.coopProb = 0;
 	}
-	
+
 	public String getType() {
 		return "Defector";
 	}
-	
+
 	public Organism reproduce() {
-		
+		PartialCooperator newOrg = new PartialCooperator();
+		for (int i = 0; i < 10; i++) {
+			this.decrementEnergy();
+		}
+		return newOrg;
 	}
-	
+
 	public double getCooperationProbability() {
 		return 0;
 	}
-	
+
 	public boolean cooperates() {
 		return false;
 	}
