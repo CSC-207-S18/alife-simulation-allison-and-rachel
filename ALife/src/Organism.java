522250637d6ed4cd85d2c775268cb324a99a7f35
AllisonBrink <35696052+AllisonBrink@users.noreply.github.com>
2018-02-16 20:43:58 -0600

    Finished all methods

--- ALife/src/Organism.java
+++ ALife/src/Organism.java
@@ -1,51 +1,42 @@
-import java.util.Map;
 
 public abstract class Organism {
 	private int energy;
-	public double coopProb;
-	
-	
+
 	public Organism() {
-		//super(null);
+		// super(null);
 		this.energy = 0;
-		this.coopProb = 0;
 	}
-	
+
 	public void update() {
 		this.energy += 1;
-		
+
 	}
-	
+
 	public int getEnergy() {
 		return this.energy;
 	}
-	
-	public void incrementEnergy () {
+
+	public void setEnergy(int newEnergy) {
+		if (newEnergy >= 0) {
+			this.energy = newEnergy;
+		}
+	}
+
+	public void incrementEnergy() {
 		this.energy += 1;
 	}
-	
-	public void decrementEnergy () {
+
+	public void decrementEnergy() {
 		if (this.energy > 0)
-		this.energy -= 1;
+			this.energy -= 1;
 	}
-	
+
 	public abstract String getType();
-	
+
 	public abstract Organism reproduce();
-	
+
 	public abstract double getCooperationProbability();
-	
+
 	public abstract boolean cooperates();
 
-
 }
-
-
-
-	
-	
-	
-	
-	
-	
-	
