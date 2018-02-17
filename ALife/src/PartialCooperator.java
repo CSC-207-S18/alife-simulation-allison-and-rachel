658e99d644929c0f15edd982acc7838379c83415
AllisonBrink <35696052+AllisonBrink@users.noreply.github.com>
2018-02-16 20:44:58 -0600

    Finished all methods

--- ALife/src/PartialCooperator.java
+++ ALife/src/PartialCooperator.java
@@ -1,26 +1,33 @@
+import java.util.Random; 
 
-public class PartialCooperator extends Organism{
-	private int energy;
+public class PartialCooperator extends Organism {
+	// private int energy;
 	public double coopProb;
-	
+
 	public PartialCooperator() {
-		
+		this.coopProb = .5;
 	}
-	
+
 	public String getType() {
 		return "PartialCooperator";
 	}
-	
+
 	public Organism reproduce() {
-		
+		PartialCooperator newOrg = new PartialCooperator();
+		for (int i = 0; i < 10; i++) {
+			this.decrementEnergy();
+		}
+		return newOrg;
 	}
-	
+
 	public double getCooperationProbability() {
 		return .5;
 	}
-	
+
 	public boolean cooperates() {
-		
+		Random numGenerator = new Random();
+		int rand = numGenerator.nextInt();
+		return (rand%2 == 0);
 	}
 
 }
