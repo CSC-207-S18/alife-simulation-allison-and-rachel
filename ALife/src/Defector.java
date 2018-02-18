664853cf7af5ea579609c30d5dcdcd6049771384
AllisonBrink <35696052+AllisonBrink@users.noreply.github.com>
2018-02-17 20:00:39 -0600

    Javadoc comments

--- ALife/src/Defector.java
+++ ALife/src/Defector.java
@@ -2,14 +2,27 @@
 public class Defector extends Organism {
 	private double coopProb;
 
+	/**
+	 * Constructor for the Defector subclass of Organism
+	 */
 	public Defector() {
 		this.coopProb = 0;
 	}
-
+	
+	/**
+	 * Returns the type of the Organism, Defector
+	 * 
+ 	 * @return "Defector", a String
+	 */
 	public String getType() {
 		return "Defector";
 	}
 
+	/**
+	 * Makes and return a new Organism of type Defector
+	 * 
+	 * @return the new Defector Organism
+	 */
 	public Organism reproduce() {
 		Defector newOrg = new Defector();
 		for (int i = 0; i < 10; i++) {
@@ -18,10 +31,20 @@ public class Defector extends Organism {
 		return newOrg;
 	}
 
+	/**
+	 * returns the probability that the Defector will cooperate, 1.0
+	 * 
+	 * @return the cooperation probability of the Defector
+	 */	
 	public double getCooperationProbability() {
 		return this.coopProb;
 	}
 
+	/**
+	 * Returns if the Defector will cooperate
+	 * 
+	 * @return false
+	 */	
 	public boolean cooperates() {
 		return false;
 	}
