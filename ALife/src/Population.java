4310d60c03484fbf74f6fca43c1c7d90fae6898b
AllisonBrink <35696052+AllisonBrink@users.noreply.github.com>
2018-02-17 19:16:46 -0600

    Finished program

--- ALife/src/Population.java
+++ ALife/src/Population.java
@@ -8,20 +8,13 @@ public class Population {
 	public Map<String, Integer> counts;
 	public ArrayList<Organism> Pop;
 	public int populationSize;
-	// public ArrayList<Organism> defectElements;
-	// public ArrayList<Organism> partialElements;
 
 	public Population(Map<String, Integer> counts) throws IllegalArgumentException {
 		this.counts = counts;
 		this.Pop = new ArrayList<Organism>();
-		// this.defectElements = new ArrayList<Organism>();
-		// this.partialElements = new ArrayList<Organism>();
 		this.tick = 0;
-		System.out.println(counts.get("Cooperator"));
-		System.out.println(counts.get("Defector"));
 		for (int i = 0; i < counts.get("Cooperator"); i++) {
 			Cooperator org = new Cooperator();
-			// maybe declare it as type Organism
 			Pop.add(org);
 		}
 		for (int i = 0; i < counts.get("Defector"); i++) {
@@ -33,13 +26,9 @@ public class Population {
 			Pop.add(org);
 		}
 		this.populationSize = counts.get("PartialCooperator") + counts.get("Defector") + counts.get("Cooperator");
-		for (int i = 0; i < populationSize; i++) {
-			System.out.println(Pop.get(i).getType());
-		}
 	}
 
 	public void update() {
-		//System.out.println(this.getPopulationCounts().get("Cooperator"));
 		for (int i = 0; i < populationSize; i++) {
 			Organism temp = Pop.get(i);
 			temp.update();
@@ -54,30 +43,23 @@ public class Population {
 			if (temp.getEnergy() >= 10) {
 				Random numGenerator = new Random();
 				int rand = Math.abs(numGenerator.nextInt());
-				//System.out.println(rand%populationSize);
-				Pop.remove(rand%populationSize);
-				System.out.printf("\n" + temp.reproduce().getType() + "\n");
-				Pop.add(temp.reproduce());
-				String orgType = new String(temp.reproduce().getType());
-				System.out.println("orgtype = " + orgType);
-				int orgNum = counts.get(temp.getType());
-				counts.put(orgType,orgNum++);
-				System.out.println("orgnum = " + orgNum);
+				int indexRemove = rand % populationSize;
+				String indexRemoveType = Pop.get(indexRemove).getType();
+				int numRemove = counts.get(indexRemoveType);
+				counts.put(indexRemoveType, numRemove - 1);
+				Pop.remove(indexRemove);
+				Organism addOrg = Pop.get(i).reproduce();
+				int numAdd = counts.get(addOrg.getType());
+				Pop.add(addOrg);
+				counts.put(addOrg.getType(), numAdd + 1);
 			}
 		}
-		//System.out.println(populationSize);
 	}
 	
 	public double calculateCooperationMean() {
 		double sum = 0;
 		for (int i = 0; i < populationSize; i++) {
-			System.out.println(Pop.get(i).getType());
-		}
-		for (int i = 0; i < populationSize; i++) {
 			sum += Pop.get(i).getCooperationProbability();
-			System.out.println(Pop.get(i).getType());
-			System.out.println(Pop.get(i).getCooperationProbability());
-			System.out.println(sum);
 		}
 		return (sum / populationSize);
 	}
